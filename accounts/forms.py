f535e12681183b3fe8ddbd64b2bb8dcb2261abed
David Barnes <dbarnes@kvcc.edu>
2022-06-03 19:34:15 -0400

    Add urls, views, and templates for home, login, and signup pages. Add base template for other templates to extend. Add form for signup. Add tests to verify signup functionality.

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,7 +8,11 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
 
 
 class CustomUserChangeForm(UserChangeForm):
@@ -16,4 +20,8 @@ class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
--- django_project/settings.py
+++ django_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "django_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -103,6 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Model to use for User Auth
 AUTH_USER_MODEL = "accounts.CustomUser"
 
+# Login and Logout
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
